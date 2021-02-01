0970eb97ef7464f70a0b5a418810ea74da0b91b7
muhamadzaimzamzami <mzaimzamzami92@gmail.com>
2021-02-01 15:33:55 +0700

    Tugas Plugin

---
+++ pertemuan6/ganjilGenap.js
@@ -0,0 +1,14 @@
+
+function ganjilGenap() {
+    
+    for (let index = 1; index <= 10; index++){
+        if (index % 2 === 0) {
+            console.log(index +" adalah bilangan Genap");
+        }else{
+            console.log(index +" adalah bilangan Ganjil");
+        }
+       
+    }
+}
+
+ganjilGenap();
---
+++ pertemuan7/bulan.js
@@ -0,0 +1,7 @@
+let bulan = ["Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"];
+
+if (bulan.length > 0) {
+    console.log(bulan.join("\n"));
+}else{
+    console.log("Nama Bulan tidak ada, isi dulu lahhh");
+}
